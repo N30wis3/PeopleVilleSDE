747026ecfd1ae60696488ee9616393394262a7d4
luca5393 <120323430+luca5393@users.noreply.github.com>
2024-10-23 12:48:26 +0200

    Added oldVillagers and nursing homes

---
+++ PeopleVilleVillagerOld/NursingHome.cs
@@ -0,0 +1,22 @@
+﻿using PeopleVilleEngine;
+using PeopleVilleEngine.Locations;
+
+namespace PeopleVilleVillagerOld;
+public class NursingHome : IHouse
+{
+    public NursingHome()
+    {
+        var random = RNG.GetInstance();
+        MaxPopulation = random.Next(3, 7);
+    }
+    private readonly List<BaseVillager> _villagers = new();
+    public string Name => $"Nursing Home, with a population of {Population}.";
+
+    public List<BaseVillager> Villagers()
+    {
+        return _villagers;
+    }
+
+    public int Population => _villagers.Count;
+    public int MaxPopulation { get; set; }
+}
