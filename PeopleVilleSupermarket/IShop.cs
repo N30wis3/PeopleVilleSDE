451d88f9a3da5fa25f5e96f1586f198f137d6c93
luca5393 <120323430+luca5393@users.noreply.github.com>
2024-10-24 12:24:36 +0200

    Added Initial Supermarket

---
+++ PeopleVilleSupermarket/IShop.cs
@@ -0,0 +1,15 @@
+﻿using PeopleVilleEngine.Locations;
+
+namespace PeopleVilleSupermarket
+{
+    public interface IShop : ILocation
+    {
+        public List<Item> Items { get; set; }
+
+    }
+
+    public class Item
+    {
+
+    }
+}
---
+++ PeopleVilleSupermarket/Supermarket.cs
@@ -0,0 +1,26 @@
+﻿using PeopleVilleEngine.Locations;
+using PeopleVilleEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleVilleSupermarket
+{
+    public class Supermarket : IShop
+    {
+        private readonly List<BaseVillager> _villagers = new();
+        public List<Item> Items { get; set; } = LoadItems();
+
+        public string Name => $"Supermarket, with {Population} villagers. Items: " + string.Join(", ", Items);
+
+        public List<BaseVillager> Villagers()
+        {
+            return _villagers;
+        }
+        public int Population => _villagers.Count;
+
+    }
+
+}
