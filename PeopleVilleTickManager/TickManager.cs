f512c3fe0a30a8cb6d6e0745ed538c175a5a5a5c
Marcus Liam Nielsen <marcus.email2016@gmail.com>
2024-10-28 09:10:55 +0100

    Update TickManager.cs
    Seperating my code into separate methods.

--- PeopleVilleTickManager/TickManager.cs
+++ PeopleVilleTickManager/TickManager.cs
@@ -1,4 +1,5 @@
 ﻿using PeopleVilleEngine;
+using PeopleVilleEngine.Locations;
 using PeopleVilleMovement;
 
 namespace PeopleVilleTickManager
@@ -23,15 +24,13 @@ namespace PeopleVilleTickManager
                 foreach (var villager in location.Villagers() )
                 {
                     float deathChance = villager.Age / 25; // TODO: Balance this
-                    if (ran.Next(0, Convert.ToInt32(Math.Round(deathChance))) == 0 || villager.Food == 0) // TODO: Seperate these two conditions into seperate statements which run the same method with different parameters
+                    if (ran.Next(0, Convert.ToInt32(Math.Round(deathChance))) == 0) // TODO: Seperate these two conditions into seperate statements which run the same method with different parameters
                     {
-                        Console.WriteLine($"{villager.FirstName} {villager.LastName} has died at the age of {villager.Age}");
-                        location.Villagers().Remove(villager);
-
-                        if (location.Villagers().Count == 0)
-                        { 
-                            Console.WriteLine($"{location.Name} has been abandoned due to lack of inhabitants, {villager.FirstName} {villager.LastName} was the last inhabitant");
-                        }
+                        Die(location, villager, "complications");
+                    }
+                    else if (villager.Food == 0)
+                    {
+                        Die(location, villager, "starvations");
                     }
 
                     if (villager.IsWorking) { villager.Food -= 2; }
@@ -39,6 +38,7 @@ namespace PeopleVilleTickManager
 
                     if (villager.Food <= 20) // TODO: Seperate into own method
                     {
+                        
                         if (villager.Money >= (100 - villager.Food) * pricePerFood)
                         {
                             villager.Money -= (100 - villager.Food) * pricePerFood;
@@ -54,6 +54,17 @@ namespace PeopleVilleTickManager
             hour++;
             await Task.Delay(50);
             Ticker(village);
+        }
+
+        void Die(ILocation location, BaseVillager villager, string cause)
+        {
+            Console.WriteLine($"{villager.FirstName} {villager.LastName} has died at the age of {villager.Age} due to {cause}.");
+            location.Villagers().Remove(villager);
+
+            if (location.Villagers().Count == 0)
+            {
+                Console.WriteLine($"{location.Name} has been abandoned due to lack of inhabitants, {villager.FirstName} {villager.LastName} was the last inhabitant");
+            }
         }
     }
 }
